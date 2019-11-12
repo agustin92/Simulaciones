04669338579aaa98c736df9419863fd54fd3e8c3
Agustin92 <agustin.lopezpedroso@gmail.com>
2019-11-12 17:57:37 -0300

    Correccion salidas, valores exactos

--- Practica_MD/write_parameters.f90
+++ Practica_MD/write_parameters.f90
@@ -7,7 +7,7 @@ real(kind=8) :: energy_tot
 select case (mode)
 case(0)
     open(unit = 45, file = 'variables_fisicas.dat', status = 'unknown')
-    write(45,*) 'Tiempo,Energia_pot,Energy_cin,Energy,Presion,Temperatura_md'
+    write(45,*) 'Tiempo,Energia_pot,Energia_cin,Energia,Presion,Temperatura_md'
 case(1)
     energy_tot = energy_cin + energy_pot
     write(45,*) dt*npaso,',',energy_pot,',',energy_cin,',',energy_tot,',',presion,',',temp_md
