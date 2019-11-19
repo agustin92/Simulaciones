ca918748c314b5025e9f71829b7e6c9c5e633ce4
Luciana <lupaumartinez@gmail.com>
2019-11-19 19:42:37 -0300

    ploteo

--- Practica_MD/plot_data_means.py
+++ Practica_MD/plot_data_means.py
@@ -42,6 +42,9 @@ def open_data_RUNS(folder):
 	presion2 = np.zeros(L)
 	presion_var = np.zeros(L)
 	temperature = np.zeros(L)
+	energia_pot = np.zeros(L)
+	energia_cin = np.zeros(L)
+        energia_mec = np.zeros(L)
 
 	for i in range(L):
 		print('Carpeta RUN:', list_of_folder[i])
@@ -56,9 +59,16 @@ def open_data_RUNS(folder):
 		presion_var[i] = data[1,4]
 		temperature[i] = data[1,5]
 
-	mean_data = np.array([np.mean(densidad), np.mean(temperature_in), np.mean(presion), np.mean(presion2), np.mean(presion_var), np.mean(temperature)]).T
+		name_energy = os.path.join(folder, list_of_folder[i], 'variables_fisicas.dat')
+		data_energy = np.genfromtxt(name_energy, delimeter = ',')
+
+                energia_pot[i] = np.mean(data[1,:])
+                energia_cin[i] = np.mean(data[2,:])
+                energia_mec[i] = np.mean(data[3,:])
+
+	mean_data = np.array([np.mean(densidad), np.mean(temperature_in), np.mean(presion), np.mean(presion2), np.mean(presion_var), np.mean(temperature), np.mean(energia_pot), np.mean(energia_cin), np.mean(energia_mec)]).T
 	name = os.path.join(folder, 'all_mean_measurement.csv')
-	header_text = '#densidad, temperatura_in, presion, presion2, presion_var, temperatura'
+	header_text = '#densidad, temperatura_in, presion, presion2, presion_var, temperatura, energia pot, energia cin, energia mec'
 	np.savetxt(name, mean_data, header=header_text)
 
 	print('Creado los valores medios de los RUNs para la carpeta.')
@@ -80,6 +90,9 @@ def open_data_step2(folder, save_folder):
 	presion2 = np.zeros(L)
 	presion_var = np.zeros(L)
 	temperature = np.zeros(L)
+        energia_pot = np.zeros(L)
+        energia_cin = np.zeros(L)
+        energia_mec = np.zeros(L)
 
 	for i in range(L):
 
@@ -94,10 +107,12 @@ def open_data_step2(folder, save_folder):
 		presion2[i] = data[3]
 		presion_var[i] = data[4]
 		temperature[i] = data[5]
-
-	data = np.array([densidad, temperature_in, presion, presion2, presion_var, temperature]).T
+                energia_pot[i] = data[6]
+                energia_cin[i] = data[7]
+                energia_mec[i] = data[8]
+	data = np.array([densidad, temperature_in, presion, presion2, presion_var, temperature, energia_pot, energia_cin, energia_mec]).T
 	name = os.path.join(save_folder, 'all_mean.csv')
-	header_text = '#densidad, temperatura_in, presion, presion2, presion_var, temperatura'
+	header_text = '#densidad, temperatura_in, presion, presion2, presion_var, temperatura. energia pot, energia cin, energia mec'
 	np.savetxt(name, data, header=header_text)
 
 	print('Juntados todos los valores medios')
@@ -122,6 +137,9 @@ def plot_data(save_folder):
 	presion2 = data[0:,3]
 	presion_var = data[0:,4]
 	temperatura = data[0:,5]
+        energia_pot = data[0:,6]
+        energia_cin = data[0:,7]
+        energia_mec = data[0:,8]
 
 	print('Grafico presion vs densidad')
 	plt.figure()
@@ -152,15 +170,27 @@ def plot_data(save_folder):
 	plt.savefig(figure_name, dpi = 400)
 	plt.close()
 
-	return
+	print('Grafico energias vs densidad')
+	plt.figure()
+	plt.plot(densidad, energia_pot, 'o', label = 'Energia potencial')
+	plt.plot(densidad, energia_cin, 'o--', label = 'Energia cinética')
+        plt.plot(densidad, energia_mec, '*', label = 'Energia mecanica')
+	plt.xlabel('Densidad')
+	plt.ylabel('Energia')
+	plt.legend(loc = 'upper rigth')
+	figure_name = os.path.join(save_folder, 'energia_vs_densidad.png')
+	plt.savefig(figure_name, dpi = 400)
+	plt.close()
 
+	return
 
 if __name__ == '__main__':
 
 	#poner la direccion Densidad"
 	#parent_folder = 'C:/Users/Alumno/Dropbox/Simulaciones-master/Practica_MD/Densidades'
-	parent_folder = '/home/alumnoit/Desktop/Simulaciones-master/Practica_MD/Densidades/Densidades_agus_completo'
-	parent_folder = os.path.normpath(parent_folder)
+	#parent_folder = '/home/alumnoit/Desktop/Simulaciones-master/Practica_MD/Densidades/Densidades_agus_completo'
+	parent_folder = 'home/alumno/Escritorio/Densidades/Densidades_agus_completo'
+
 	print('directorio:', parent_folder)
 	
 	save_folder = manage_save_directory(parent_folder, 'figuras_densidad')
